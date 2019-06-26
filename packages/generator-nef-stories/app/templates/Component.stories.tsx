1fc54695993613271f0f10eb35b1532ad7d044ed
Haldun Anil <haldunanil@users.noreply.github.com>
2019-06-26 14:41:03 -0700

    feat: improve stories generator template syntax

--- packages/generator-nef-stories/app/templates/Component.stories.tsx
+++ packages/generator-nef-stories/app/templates/Component.stories.tsx
@@ -6,13 +6,11 @@ import <%= name %>Readme from "./README.md";
 
 import <%= name %> from ".";
 
-const <%= name %>Stories = storiesOf("<%= name %>", module);
-
-// add the knobs as a decorator and readme as a param
-<%= name %>Stories.addDecorator(withKnobs).addParameters({
-  readme: { content: <%= name %>Readme }
-});
-
-<%= name %>Stories.add("basic use", () => <<%= name %> />);
+const <%= name %>Stories = storiesOf("<%= name %>", module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    readme: { content: <%= name %>Readme }
+  })
+  .add("basic use", () => <<%= name %> />);
 
 export default <%= name %>Stories;
