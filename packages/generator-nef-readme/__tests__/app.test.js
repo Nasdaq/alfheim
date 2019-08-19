bcb55a6d879b65fee31b1f8ffd371cd326418d7d
Haldun Anil <haldunanil@gmail.com>
2019-08-18 21:25:43 -0400

    feat: add tests for `generator-nef-readme` (#13)

---
+++ packages/generator-nef-readme/__tests__/app.test.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const helpers = require("yeoman-test");
+const assert = require("yeoman-assert");
+const rimraf = require("rimraf");
+
+describe(`generator-nef-readme`, () => {
+  const componentName = "Test";
+
+  afterEach(() => {
+    rimraf.sync(path.join(__dirname, "tmp"));
+  });
+
+  describe(`if 'relative-path' option is defined`, () => {
+    const relPath = "some-path/";
+    const filePath = path.join(
+      __dirname,
+      `tmp/${relPath}${componentName}/README.md`
+    );
+    beforeEach(() => {
+      return helpers
+        .run(path.join(__dirname, "../app"))
+        .inDir(path.join(__dirname, "tmp"))
+        .withArguments([componentName])
+        .withOptions({ "relative-path": relPath });
+    });
+
+    it(`should nest the component folder into that path`, () => {
+      assert.file(filePath);
+    });
+
+    it(`should have the name of the component as an H1`, () => {
+      assert.fileContent(filePath, `# ${componentName}`);
+    });
+
+    it(`should have a basic intro paragraph`, () => {
+      assert.fileContent(
+        filePath,
+        `\`${componentName}\` is an easy-to-use component.`
+      );
+    });
+
+    it(`should create a basic usage import example`, () => {
+      assert.fileContent(
+        filePath,
+        `import { ${componentName} } from "@nef/react";`
+      );
+    });
+  });
+
+  describe(`if 'relative-path' option is NOT defined`, () => {
+    const filePath = path.join(__dirname, `tmp/${componentName}/README.md`);
+
+    beforeEach(() => {
+      return helpers
+        .run(path.join(__dirname, "../app"))
+        .inDir(path.join(__dirname, "tmp"))
+        .withArguments([componentName]);
+    });
+
+    it(`should generate a file named 'README.md'`, () => {
+      assert.file(filePath);
+    });
+
+    it(`should have the name of the component as an H1`, () => {
+      assert.fileContent(filePath, `# ${componentName}`);
+    });
+
+    it(`should have a basic intro paragraph`, () => {
+      assert.fileContent(
+        filePath,
+        `\`${componentName}\` is an easy-to-use component.`
+      );
+    });
+
+    it(`should create a basic usage import example`, () => {
+      assert.fileContent(
+        filePath,
+        `import { ${componentName} } from "@nef/react";`
+      );
+    });
+  });
+});
