37a33e677a86f297c2d1e47b5536869d7fd23b38
Haldun Anil <haldun.anil@nasdaq.com>
2019-09-06 11:37:31 -0400

    feat(`cli`): add `generator-nef-library` to `init` function of the cli

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -24,7 +24,7 @@ switch (script) {
     const result = spawn.sync(
       "yo",
       nodeArgs
-        .concat('@alfheim/nef-component')
+        .concat("@alfheim/nef-component")
         .concat(args.slice(scriptIndex + 1)),
       { stdio: "inherit" }
     );
@@ -51,6 +51,35 @@ switch (script) {
     break;
 
   case "init":
+    const result = spawn.sync(
+      "yo",
+      nodeArgs
+        .concat("@alfheim/nef-library")
+        .concat(args.slice(scriptIndex + 1)),
+      { stdio: "inherit" }
+    );
+
+    if (result.signal) {
+      if (result.signal === "SIGKILL") {
+        log(`
+            The build failed because the process exited too early.
+            This probably means the system ran out of memory or someone called
+            kill -9" on the process.
+          `);
+      } else if (result.signal === "SIGTERM") {
+        log(
+          chalk.red(`
+               The build failed because the process exited too early. 
+               Someone might have called "kill" or "killall", or the system could 
+               be shutting down.
+             `)
+        );
+      }
+      process.exit(1);
+    }
+    process.exit(result.status);
+    break;
+
   case undefined:
     break;
 
